eede2cdb2fb5bf728a262328777f2ec1b60b09ec
yh3790 <yh3790@gmail.com>
2022-12-22 23:07:40 +0900

    로그아웃 로직

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const config = require('./config/key')
 const { User } = require("./models/User")
-
+const { auth } = require("./middleware/auth")
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
     extend:true
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 })
 
 
-app.post("/register",(req,res) => {
+app.post("/api/users/register",(req,res) => {
     //회원 가입할때 필요한 정보들 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
     //req.body는 클라이언트에서 입력된  id,pd 등을 가져오는데 이게 가능한이유는 bodypaser임포트했기떄문
     const user = new User(req.body)
@@ -39,7 +39,7 @@ app.post("/register",(req,res) => {
     })
 })
 
-app.post('/login',(req,res) => {
+app.post('/api/users/login',(req,res) => {
   User.findOne({email: req.body.email},(err,user)=>{
     if(!user){
       return res.json({
@@ -65,6 +65,28 @@ app.post('/login',(req,res) => {
   })
 })
 
+app.get('/api/users.auth', auth ,(req,res)=>{
+  //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True 라는말.
+  res.status(200).json({
+    _id : req.user._id,
+    isAdmin : req.user.role === 0 ? false : true,
+    isAuth : true,
+    email : req.user.email,
+    name : req.user.name,
+    lastname : req.user.lastname,
+    role : req.uesr.role
+  })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/api/users/logout',auth,(res,req)=>{
+  User.findOneAndUpdate({ _id : req.user._id},{token:""},(err,user)=>{
+    if(err) return res.json({success : false ,err});
+    return res.status(200).send({
+      success : true
+    })
+  })
 })
